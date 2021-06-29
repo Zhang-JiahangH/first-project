ce2301bc92ca0244e663b29a98c28e3a153e7522
Ryan Zhou <ryanzhou7@gmail.com>
2021-06-29 16:10:18 -0700

    Update gitignore, add assignment 1/2 stubs

---
+++ src/assignment/Assignment1.java
@@ -0,0 +1,11 @@
+package assignment;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello world!");
+	}
+
+}
