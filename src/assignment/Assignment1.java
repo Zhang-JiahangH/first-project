a0d3128cb1c724ab7822e547445d568019833f7a
Jiahang Zhang <jiahang@usc.edu>
2021-07-15 11:30:55 +0800

    This works!

--- src/assignment/Assignment1.java
+++ src/assignment/Assignment1.java
@@ -4,8 +4,9 @@ public class Assignment1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("This might work.");
 		System.out.print("Hello world!");
+
 	}
 
 }
