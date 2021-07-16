54bb7867c4b458979e33c184ebfc3ee904cf77dc
Ryan Zhou <ryanzhou7@gmail.com>
2021-07-15 21:19:49 -0700

    Add textbook problems

---
+++ src/text/chapter11/pa5/Course.java
@@ -0,0 +1,47 @@
+package text.chapter11.pa5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+    private String courseName;
+    private List<String> students = new ArrayList<>();
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void addStudent(String student) {
+        students.add(student);
+    }
+
+    public List<String> getStudents() {
+        return students;
+    }
+
+    public int getNumberOfStudents() {
+        return students.size();
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void dropStudent(String student) {
+        students.remove(student);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Course: ");
+        stringBuilder.append(courseName);
+        stringBuilder.append( "students[ ");
+        for (String student : students) {
+            stringBuilder.append(student);
+            stringBuilder.append(" ");
+        }
+        stringBuilder.append( "]");
+        return stringBuilder.toString();
+    }
+}
---
+++ src/text/chapter11/pa5/Problem.java
@@ -0,0 +1,29 @@
+package text.chapter11.pa5;
+
+public class Problem {
+
+    /*
+    11.5 (The Course class) Rewrite the Course class in Listing 10.6. Use an ArrayList to replace an array to store students.
+    You should not change the original contract of the Course class
+    (i.e., the defi- nition of the constructors and methods should not be changed, but the private members may be changed.)
+    */
+    public static void main(String[] args) {
+
+        Course math = new Course("Calculus");
+        System.out.println(math);
+
+        String bob = "Bob";
+        math.addStudent("Bob");
+        System.out.println(math);
+
+        math.addStudent("Joe");
+        System.out.println(math);
+
+        System.out.println(math.getCourseName());
+
+        System.out.println(math.getNumberOfStudents());
+
+        math.dropStudent(bob);
+        System.out.println(math);
+    }
+}
