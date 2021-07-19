7b6309ebeea6447527f3784a5878c09a40296549
Jiahang Zhang <jiahang@usc.edu>
2021-07-19 10:34:58 +0800

    Trial on problem 4

--- src/text/chapter11/pe4/Problem.java
+++ src/text/chapter11/pe4/Problem.java
@@ -1,4 +1,6 @@
 package text.chapter11.pe4;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Problem {
 
@@ -10,6 +12,36 @@ public class Problem {
      with 0 and invokes this method to return the largest number in the input.
      */
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        int value;
 
+        do{
+            System.out.print("Enter integers (input ends with 0): ");
+            value = input.nextInt();
+            list.add(value);
+        } while (value != 0);
+
+        int result = max(list);
+        System.out.println("The maximum number is " + result);
+    }
+
+    // function //
+    public static int max(ArrayList<Integer> list){
+        int len = list.size();
+        int max;
+        if(len == 0){
+            return -1;
+        }
+        else{
+            max = list.get(0);
+        }
+
+        for(int temp:list){
+            if(temp > max){
+                max = temp;
+            }
+        }
+        return max;
     }
 }
